163a803adc89a62f8997070cea1eeb3c83f953ca
kameush <nico9017@gmail.com>
2018-05-11 20:11:34 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,13 +14,33 @@ const publicPath = path.join(__dirname, '../public')
 io.on('connection', (socket) => {
   console.log('New user connected')
 
+  // socket.emit from admin text welcome to the cat app
+  //  socket.broadcast.emit from Admin text New user joined
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (msg) => {
     console.log('Message from client: ', msg)
+
     io.emit('newMessage', {
       from: msg.from,
       text: msg.text,
       createdAt: new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage', {
+    //   from: msg.from,
+    //   text: msg.text,
+    //   createdAt: new Date().getTime()
+    // })
   })
 
   socket.on('disconnect', () => {
